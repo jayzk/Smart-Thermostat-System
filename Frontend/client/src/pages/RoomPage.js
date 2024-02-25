4089b6231ea4de5228784545f7e19474c69ace43
Jayden <jaydenzkyeo@gmail.com>
2024-02-24 18:59:39 -0700

    room number can now be changed via login page

--- Frontend/client/src/pages/RoomPage.js
+++ Frontend/client/src/pages/RoomPage.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+//import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
 
 function RoomPage() {
   const [desiredTemp, setDesiredTemp] = useState(20);
   const [currentTemp, setCurrentTemp] = useState(22);
-  const [roomNum, setRoomNum] = useState(1);
+  //const [roomNum, setRoomNum] = useState(useParams());
+  let {roomNum} = useParams();
 
   // Function to increment desired temperature
   const incrementDesiredTemp = () => {
