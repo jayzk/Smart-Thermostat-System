bd3a478a53f24e7af166f706a858152bed9d2c0f
tanmayMishra17 <119141252+tanmayMishra17@users.noreply.github.com>
2024-03-19 19:15:45 -0600

    Updated Proxy Server (Need to run and check again)

--- Backend/thermostatSystem/src/main/java/backend/Proxy/ProxyServer.java
+++ Backend/thermostatSystem/src/main/java/backend/Proxy/ProxyServer.java
@@ -8,6 +8,7 @@ import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 
 @RestController
@@ -16,6 +17,7 @@ public class ProxyServer {
     private String centralServerAddress;
     private ArrayList<Integer> serverPorts;
     private int loadIndex;
+    private int leaderPort;
 
     public ProxyServer() {
         this.centralServerAddress = "127.0.0.1";
@@ -25,6 +27,7 @@ public class ProxyServer {
         this.serverPorts.add(10002);
         this.serverPorts.add(10003);
         this.loadIndex = 0;
+        this.leaderPort = -1;
     }
 
 
@@ -42,6 +45,8 @@ public class ProxyServer {
         
         // Get avalible server ports
         ArrayList<Integer> avalibleServerPorts = checkAvalible(checkMessage);
+        
+        
 
         // Find the port post request
         int centralServerPort;
@@ -81,8 +86,54 @@ public class ProxyServer {
         return "ACK from proxy server";
     }
 
-    public ArrayList<Integer> checkAvalible(String checkMessage){
-        ArrayList<Integer> avalibleServerPorts = new ArrayList<Integer>();
+    public int makeLeader(ArrayList<Integer> availableServers){
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(availableServers.size());
+
+        leaderPort = availableServers.get(randomIndex);
+        for(int i = 0; i < serverPorts.size(); i++){
+            try {
+                String respond = "";
+                Socket socket = new Socket(centralServerAddress, serverPorts.get(i));
+                System.out.println("Check replica Alive: replica port " + serverPorts.get(i));
+
+                // Create output stream to send request
+                OutputStream outputStream = socket.getOutputStream();
+                PrintWriter out = new PrintWriter(outputStream, true);
+
+                // Send request to the server
+                out.println("Leader =" + Integer.toString(leaderPort));             //NOTE: NOT IMPLEMENTED IN SERVERAPPLICATION YET
+                
+                // Create input stream to receive response
+                InputStream inputStream = socket.getInputStream();
+                BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+                respond = in.readLine();
+                // System.out.println("Respond: " + respond);
+                
+                if (serverPorts.get(i) == leaderPort){
+                    if(!respond.equals("Ok")){
+                        initiateReElection();
+                    }
+                }
+
+                          
+                out.close();
+                in.close();
+                socket.close();
+            } catch (IOException e) {
+                System.out.println("Port " + serverPorts.get(i) + " is not alive.");
+            }
+        }
+
+        return 1; 
+    }
+
+    private void initiateReElection() {                     // RE ELECTION INITIATE
+        System.out.println("Re-Election Initiated");
+        String checkMessage = "{ \"type\": 2 }";                                    //TYPE 2 to just check if they are alive. 
+        ArrayList<Integer> avalibleServerPorts2 = new ArrayList<Integer>();
+        
+        
         // Check avaliable ports
         for(int i = 0; i < serverPorts.size(); i++){
             try {
@@ -103,6 +154,48 @@ public class ProxyServer {
                 respond = in.readLine();
                 // System.out.println("Respond: " + respond);
                 if(respond.equals("Alive")){
+                    avalibleServerPorts2.add(serverPorts.get(i));
+                    System.out.println("Port " + serverPorts.get(i) + " is alive.");
+                }               
+                out.close();
+                in.close();
+                socket.close();
+            } catch (IOException e) {
+                System.out.println("Port " + serverPorts.get(i) + " is not alive.");
+            }
+        }
+
+        makeLeader(avalibleServerPorts2);    // Making leader randomly based on alive dudes
+
+    }
+
+
+    public ArrayList<Integer> checkAvalible(String checkMessage){
+        ArrayList<Integer> avalibleServerPorts = new ArrayList<Integer>();
+        // Check avaliable ports
+        boolean leaderAlive = false;
+        for(int i = 0; i < serverPorts.size(); i++){
+            try {
+                String respond = "";
+                Socket socket = new Socket(centralServerAddress, serverPorts.get(i));
+                System.out.println("Check replica Alive: replica port " + serverPorts.get(i));
+
+                // Create output stream to send request
+                OutputStream outputStream = socket.getOutputStream();
+                PrintWriter out = new PrintWriter(outputStream, true);
+
+                // Send request to the server
+                out.println(checkMessage);
+                
+                // Create input stream to receive response
+                InputStream inputStream = socket.getInputStream();
+                BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+                respond = in.readLine();
+                // System.out.println("Respond: " + respond);
+                if(respond.equals("Alive")){
+                    if(serverPorts.get(i)==leaderPort){
+                        leaderAlive = true;
+                    }
                     avalibleServerPorts.add(serverPorts.get(i));
                     System.out.println("Port " + serverPorts.get(i) + " is alive.");
                 }               
@@ -113,6 +206,9 @@ public class ProxyServer {
                 System.out.println("Port " + serverPorts.get(i) + " is not alive.");
             }
         }
+        if (!leaderAlive){
+            initiateReElection();          
+        } 
         return avalibleServerPorts;
     }
 
